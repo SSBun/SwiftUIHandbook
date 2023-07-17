1502727089e1c6e4817868f0e2d5d8811bfde4fd
caishilin <caishilin@zhihu.com>
2023-07-17 17:55:01 +0800

    Initial Commit

---
+++ SwiftUIHandbook/SwiftUIHandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHandbookApp.swift
+//  SwiftUIHandbook
+//
+//  Created by caishilin on 2023/7/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
